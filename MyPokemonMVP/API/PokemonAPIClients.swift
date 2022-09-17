2ea7ebc80ce112757016036a65c86b098a369ee6
ShoheiTakino <shohei0522penguin@gmail.com>
2022-09-17 23:20:37 +0900

    addfiles

---
+++ MyPokemonMVP/API/PokemonAPIClients.swift
@@ -0,0 +1,14 @@
+//
+//  Clients.swift
+//  MyPokemonMVP
+//
+//  Created by 滝野翔平 on 2022/09/17.
+//
+
+import Foundation
+
+//https://pokeapi.co/api/v2/item/234/
+
+final class PokemonAPIClients {
+    
+}
--- MyPokemonMVP/Module/MainView/Presenter.swift
+++ MyPokemonMVP/Module/MainView/Presenter.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+protocol MainViewPresenterInput: AnyObject {
+    
+}
+
+protocol MainViewPresenterOutput: AnyObject {
+    
+}
+
+final class MainViewPresenter {
+    
+}
