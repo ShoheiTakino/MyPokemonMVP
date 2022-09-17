385e7f16a66fdb9ea5b8b511564fb31c2315f174
ShoheiTakino <shohei0522penguin@gmail.com>
2022-09-18 00:05:20 +0900

    APIファイル変更

--- MyPokemonMVP/API/PokemonAPIClients.swift
+++ MyPokemonMVP/API/PokemonAPIClients.swift
@@ -10,6 +10,20 @@ import Foundation
 //https://pokeapi.co/api/v2/item/234/
 
 final class PokemonAPIClients {
-    var id: Int = 1
-    let url = URL(string: "https://pokeapi.co/api/v2/item/1/")
+    
+    func pokemonList() async throws -> [Pokemon] {
+        let id = Constants.pokemonIdRange
+        let url = URL(string: "https://pokeapi.co/api/v2/item/\(id)/")!
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in  //非同期で通信を行う
+            guard let data = data else { return }
+            do {
+                let object = try JSONSerialization.jsonObject(with: data, options: [])  // DataをJsonに変換
+                print(object)
+            } catch let error {
+                print(error)
+            }
+        }
+        task.resume()
+    }
 }
