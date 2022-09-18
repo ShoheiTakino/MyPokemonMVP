a8a3ef066dd4de2a1e0d6b96ebbb73c58db567f7
ShoheiTakino <shohei0522penguin@gmail.com>
2022-09-18 09:27:54 +0900

    cell準備

--- MyPokemonMVP/Module/MainView/Cell/MainViewControllerCell.swift
+++ MyPokemonMVP/Module/MainView/Cell/MainViewControllerCell.swift
@@ -8,5 +8,19 @@
 import UIKit
 
 final class MainViewControllerCell: UICollectionViewCell {
+
+    let identifire = "MainViewControllerCell"
+    func nib()-> UINib {
+        UINib(nibName: identifire, bundle: nil)
+    }
+    
+    @IBOutlet private weak var pokemonId: UILabel!
+    @IBOutlet private weak var pokemonName: UILabel!
+    @IBOutlet private weak var pokemonImage: UIImageView!
+    
+    func configure() {
+        
+    }
+    
     
 }
