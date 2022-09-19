f4a16328e83ec98a81b5cbe30f8a141229ce5e29
ShoheiTakino <shohei0522penguin@gmail.com>
2022-09-19 21:12:33 +0900

    API周りの実装

--- MyPokemonMVP/API/PokemonAPIClients.swift
+++ MyPokemonMVP/API/PokemonAPIClients.swift
@@ -11,7 +11,7 @@ import Foundation
 
 final class PokemonAPIClients {
     
-    func pokemonList() async throws -> [Pokemon] {
+    func pokemonList() async throws -> [PokemonList] {
         let ids = Constants.pokemonIdRange
         for id in ids {
             let url: URL = URL(string: "https://pokeapi.co/api/v2/item/\(id)/")!
@@ -20,9 +20,27 @@ final class PokemonAPIClients {
             let session: URLSession = URLSession.shared
             let task: URLSessionDataTask = session.dataTask(with: request, completionHandler: {( data, response, error) in
                 
-                let res: HTTPURLResponse = response as! HTTPURLResponse
-                print(res.statusCode)
+                guard let data = data else { return }
+                
+                if error != nil {
+                    print(error ?? "")
+                    return
+                }
+                do{
+                    let decoder = JSONDecoder()
+                    let resultList = try decoder.decode(PokemonList.self, from: data)
+                    
+                    for pokemon in resultList.results {
+                        print(pokemon.id)
+                        print(pokemon.name)
+                        print(pokemon.url)
+                        PokemonList.append(pokemon)
+                    }
+                } catch {
+                    print("解析中にerror")
+                }
             })
+            task.resume()
         }
     }
 }
